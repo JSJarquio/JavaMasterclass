b62df648a694ba170af081cf702c2bb5b0420b16
Jordan Jarquio <jsjarquio@gmail.com>
2018-07-21 15:58:10 -0500

    Finishing up the ForLoop

--- ForLoop/src/com/JordanJ/Main.java
+++ ForLoop/src/com/JordanJ/Main.java
@@ -9,17 +9,17 @@ public class Main {
 //        for(initialize; termination; increment) {
 //    }
 
-        for (int i = 6; i < 5; i++) {
+        for (int i = 6; i < 1; i++) {
             System.out.println("Loop " + i + " hello!");
         }
 
-        for (int j = 2; j <= 8; j++) {
+        for (int j = 2; j <= 1; j++) {
             System.out.println("$10,000 at " + j + "% interest = $" + String.format("%.2f",calculatedInterest(10000.0, j)));
         }
 
         System.out.println("********************************");
 
-        for (int h = 8; h >= 2; h--) {
+        for (int h = 8; h >= 8; h--) {
             System.out.println("$10,000 at " + h + "% interest = $" + String.format("%.2f",calculatedInterest(10000.0, h)));
         }
 
@@ -28,12 +28,28 @@ public class Main {
             if (isPrime(i)) {
                 count++;
                 System.out.println("Number " + i + " is a prime number.");
-                if (count == 100) {
+                if (count == 1) {
                     System.out.println("Exiting for loop");
                     break;
                 }
             }
         }
+
+        int n = 1;
+        int sumNumbers = 0;
+        for (int i = 1; i <= 1000; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                if (n <= 5) {
+                    n++;
+                    sumNumbers += i;
+                    System.out.println(i + " can be divided by 3 & 5.");
+                } else {
+                    break;
+                }
+            }
+        }
+
+        System.out.println(sumNumbers + " equals the total.");
 
     }
 
